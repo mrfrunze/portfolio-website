9547b85a03cb185b705b606b712ad01c76e8b735
Ilya <stoynov.oleg@gmail.com>
2024-04-11 17:45:43 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,27 @@ window.addEventListener("scroll", function(){
 })
 
 //Portfolio section - Modal
+const serviceModal = document.querySelectorAll(".service-modal")
+const learnMoreBtn = document.querySelectorAll(".learn-more-btn")
+const modalCloseBtn = document.querySelectorAll(".modal-close-btn")
+
+const modal = function(modalClick){
+    serviceModal[modalClick].classList.add("active")
+}
+
+learnMoreBtn.forEach((button, i) => {
+    button.addEventListener("click", () => {
+        modal(i)
+    })
+})
+
+modalCloseBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        serviceModal.forEach(modalView => {
+            modalView.classList.remove("active")
+        })
+    })
+})
 
 //Our clients - Swiper
 
